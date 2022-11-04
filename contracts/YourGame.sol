1f20a2f0cd41bd59ed30e521991979485f6d05b0
PumingFang <55150042+PumingFang@users.noreply.github.com>
2022-11-04 07:11:05 -0700

    Create YourGame.sol

---
+++ contracts/YourGame.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+import '@thundercore/referral-solidity/contracts/Referral.sol';
+
+contract YourGame is Referral {
+  // Referral(decimals, referralBonus, secondsUntilInactive, onlyRewardActiveReferrers, levelRate, refereeBonusRateMap)
+  constructor() Referral (10000, 500, 1 days, true, [6000, 3000, 1000], [1, 10000]) public {
+  }
+
+  // bind uplineAddr as msg.sender's referrer
+  function addUpline(address payable uplineAddr) public {
+    addReferrer(upline);
+  }
+
+  // trigger pay referral in your business logic
+  function play() public payable {
+    payReferral(msg.value);
+  }
+}
